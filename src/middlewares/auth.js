7aa22db87b6c86d1eea4374d0ec56a8d97776826
ismailmail123 <ismailbary2@gmail.com>
2026-03-18 00:58:14 +0800

    add limit opset

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,35 +1,3 @@
-// const jwt = require("jsonwebtoken");
-
-// const validateToken = (req, res, next) => {
-//     try {
-//         console.log("Cookies:", req.cookies);
-
-//         const token = req.cookies.jwt;
-
-//         if (!token) {
-//             return res.status(401).json({ message: "Access denied. No token provided." });
-//         }
-
-//         const userData = jwt.verify(token, process.env.JWT_SECRET);
-//         console.log("Decoded User:", userData);
-
-//         req.user = userData;
-//         next();
-//     } catch (error) {
-//         console.error("JWT Validation Error:", error);
-
-//         if (error.name === "JsonWebTokenError") {
-//             return res.status(401).json({ message: "Invalid token" });
-//         } else if (error.name === "TokenExpiredError") {
-//             return res.status(401).json({ message: "Token expired" });
-//         }
-
-//         return res.status(500).json({ message: "Internal Server Error" });
-//     }
-// };
-
-// module.exports = { validateToken };
-
 const jwt = require("jsonwebtoken");
 const user = require("../models/user");
 
